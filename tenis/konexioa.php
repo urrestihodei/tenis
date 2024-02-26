b5ddb1f80dc5f371289c768c54f46af6853688cf
Hodei <146727802+urrestihodei@users.noreply.github.com>
2024-02-26 11:26:42 +0100

    Add files via upload

---
+++ tenis/konexioa.php
@@ -0,0 +1,18 @@
+<?php
+
+    session_start();
+
+    $zerbitzaria 	= 'localhost'; // SQL Zerbitzaria
+    $erabiltzailea 	= 'root'; 			// Konexioa egingo duen erabiltzailea
+    $password 		= ''; 				// Erabiltzailearen pasahitza
+    $db 			= 'mydb';		// Konexioa erabiliko duen datubasea
+    $conn 			= new mysqli($zerbitzaria, $erabiltzailea, $password, $db);
+
+    if ($conn->connect_error) { // Konexioan erroreak ez daudela konprobatu
+        $_SESSION["error"] = "DBarekin konektatzerakoan akatsa: " . $conn->connect_error;
+        die;
+    }
+
+    
+
+?>
